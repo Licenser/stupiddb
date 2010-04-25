1cf04efb9755520cc7bfc584bc1385fd1d2ef0f2
Rayne <DiscipleRayne@gmail.com>
2010-04-25 17:31:08 +0800

    Fixed deps.

--- project.clj
+++ project.clj
@@ -1,1 +1,6 @@
-(defproject stupiddb "0.1.2" :description "A very simple database in pure clojure." :dependencies ([org.clojure/clojure "[1.1,1.2]"] [org.clojure/clojure-contrib "[1.1,1.2]"]) :dev-dependencies ([swank-clojure "1.2.0-SNAPSHOT"] [lein-search "0.3.0-SNAPSHOT"]))
+(defproject stupiddb "0.1.3" 
+  :description "A very simple database in pure clojure." 
+  :dependencies [[org.clojure/clojure "[1.1,1.2-master-SNAPSHOT]"] 
+		 [org.clojure/clojure-contrib "[1.1,1.2-SNAPSHOT]"]] 
+  :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"] 
+		     [lein-search "0.3.0-SNAPSHOT"]])
