7f7ea9ad2ae6e0bae94912ba6b38b40d2d43c420
Raynes <DiscipleRayne@gmail.com>
2010-07-21 10:56:53 +0800

    Got rid of the redundant version range.

--- project.clj
+++ project.clj
@@ -1,1 +1,7 @@
-(defproject stupiddb "0.3.0" :description "A very simple database in pure clojure." :dependencies [[org.clojure/clojure "[1.1,1.2-master-SNAPSHOT]"] [org.clojure/clojure-contrib "[1.1,1.2-SNAPSHOT]"]] :dev-dependencies ([lein-clojars "0.5.0"] [swank-clojure "1.2.0-SNAPSHOT"] [lein-search "0.3.0-SNAPSHOT"]))
+(defproject stupiddb "0.3.0"
+  :description "A very simple database in pure clojure."
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]
+                     [swank-clojure "1.2.1"]
+                     [lein-search "0.3.0-SNAPSHOT"]])
