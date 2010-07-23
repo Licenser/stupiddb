3690808f3da052f315f21d2d3a06ecfdd3ca5874
Heinz N. Gies <heinz@licenser.net>
2010-07-23 07:34:13 +0200

    Updated to new lein search version.
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- project.clj
+++ project.clj
@@ -1,7 +1,1 @@
-(defproject stupiddb "0.3.0"
-  :description "A very simple database in pure clojure."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]
-                     [swank-clojure "1.2.1"]
-                     [lein-search "0.3.0-SNAPSHOT"]])
+(defproject stupiddb "0.3.1" :description "A very simple database in pure clojure." :dependencies [[org.clojure/clojure "1.1.0"] [org.clojure/clojure-contrib "1.1.0"]] :dev-dependencies [[lein-clojars "0.5.0"] [swank-clojure "1.2.1"] [lein-search "0.3.0"]])
