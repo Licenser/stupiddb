a5d56a51bdc847bf5e51110a5ef1074172891ada
Gereon Frey <me@gereonfrey.de>
2012-02-12 21:48:16 +0100

    improved documentation

--- src/stupiddb/core.clj
+++ src/stupiddb/core.clj
@@ -1,9 +1,32 @@
+;; ## StupidDB - A very simple DB in clojure.
+;;
+;; StupidDB is a simple thread safe database handler serializing a
+;; clojure ref to a file. Keep in mind that concurrent access is only
+;; possible within one thread! Different processes are _not_ supported.
+;;
+;; A database is generated using the *db-init* function and closed
+;; using *db-close* (see [this section](#db-handling) for further
+;; information). Data can be retrieved using *db-get* and *db-get-in*
+;; for flat or nested structures respectively. The data can be
+;; manipulated using the following functions:
+;;
+;;  - *db-assoc* or *db-assoc-in* to assoc a given key with a given value,
+;;  - *db-update-in* to update the value at the given location using a
+;;    given function,
+;;  - *db-dissoc* or *db-dissoc-in* to remove a key value pair at a
+;;    given location.
+;;
+;; All functions for data retrieval and manipulation are described in
+;; [this section](#db-manip).
 (ns stupiddb.core
+  "Core namespace."
   (:import [java.io File PrintWriter PushbackReader FileInputStream FileOutputStream InputStreamReader]
            [java.util.zip GZIPInputStream GZIPOutputStream])
   (:require [clojure.java.io :as io]))
 
+;; ## File IO Helpers
 
+;; Internal functions used for writing and reading the required files.
 (defn- dependant-writer [db arg]
   (if (:gzip db)
     (PrintWriter. (GZIPOutputStream. (FileOutputStream. arg)))
@@ -26,57 +49,62 @@
   (.close out)
   (io/writer (:log-file db)))
 
+;; <a name="db-manip" />
+;; ## Database Manipulators
+
 (defn db-get
-  "Returns the value mapped to key in the db, nil or default if key is
-   not present."
+  "Returns the value mapped to *key* in *db*; nil or *default* is
+   returned if the *key* is not present."
   ([db key default] 
     (or (get-in @db [:data key]) default))
   ([db key]
      (db-get db key nil)))
 
 (defn db-get-in
-  "Returns the value that is behind the vecotr ks in a nested map"
+  "Returns the value that is behind the vector *ks* in a nested *db*."
   [db ks]
   (get-in @db (vec (concat [:data] ks))))
 
 (defn db-assoc
-  "Associates key with value in the db."
+  "Associates *key* with *value* in the given *db*."
   [db key value]
   (dosync 
    (send (:log @db) write-log  :assoc key value)
    (alter db update-in [:data] assoc key value)))
 
 (defn db-dissoc
-  "Dissociates key in db."
+  "Removes the *key* and its value from the *db*."
   [db key]
   (dosync
       (send (:log @db) write-log :dissoc key nil)
       (alter db update-in [:data] dissoc key)))
 
 (defn db-assoc-in
-  "Associates a value in a nested map in the db. (same as update-in
-   with constatly)."
+  "Associates *value* in a nested *db* at the location given in *ks*."
   [db ks value]
   (dosync
    (send (:log @db) write-log :assoc-in ks value)
    (alter db update-in (vec (concat [:data] ks)) (constantly value))))
 
 (defn db-dissoc-in
-  "Dissociates a key in a nested map in the db."
+  "Remove the *key* at *ks* in a nested *db*."
   [db ks key]
   (dosync
    (send (:log @db) write-log :dissoc-in [ks key] nil)
    (alter db update-in (concat [:data] ks) dissoc key)))
 
 (defn db-update-in
-  "Gets the value from a nested vector in db that is behind the kys ks
-   then applys f with the value as first and args as following
-   arguments and sets the new value"
+  "Update the value at the location given in *ks* in *db* by applying
+   the function *f* with the current value and the given arguments
+   *args*."
   [db ks f & args]
   (dosync
    (let [v (apply f (db-get-in db ks) args)]
      (db-assoc-in db ks v))))
 
+;; ## File Handling
+
+;; Some internal functions for handling the log and actual db file.
 (defn- handle-log [db [action key value]]
   (cond
    (= action :assoc) (update-in db [:data] assoc key value)
@@ -115,10 +143,16 @@
 		      out) @db)
       (send (:log @db) new-log @db)))
 
+;; <a name="db-handling" />
+;; ## Database Handling
+
 (defn db-init
-  "Initializes a db ref. If the db file (or file.log) already exists loads the data.
-   The db is saved every time seconds, so a log is keeped for any
-   manipulation to reconstruct data in the case of a crash."
+  "Initializes a db ref. If the file containing the actual db or logs
+   given by *file* already exists loads the contained data. The db is
+   saved every *time* seconds using a dedicated thread. For crash
+   handling a journal is kept in a log file, so that data can be
+   reconstructed in case of a crash. The database is compressed if
+   *gzip* is set to true."
   [file time & {gzip :gzip}]
   (let [gzip (boolean gzip)
         r {:data {}
@@ -138,8 +172,8 @@
     r))
 
 (defn db-close
-  "Closes a db, stops the auto saving and writes the entire log into
-   the db file for faster startup."
+  "Closes the given *db*; this includes stopping the thread used for
+   auto saving and flushes all remaining actions for a faster startup."
   [db]
   (.stop (:thread @db))
   (flush-db db)
