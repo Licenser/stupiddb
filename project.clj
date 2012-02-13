e710157d48cca1ac48f352b54a4d7f119de01364
Heinz N. Gies <heinz@licenser.net>
2012-02-13 21:34:05 +0100

    added lein-clojars to dev deps.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject stupiddb "0.4.0"
   :description "A very simple database in pure clojure." 
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
